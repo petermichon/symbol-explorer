05e94c14858c53a4bce544e0652d7fc596167662
Peter Michon <peter.michon@proton.me>
2026-07-31 00:45:49 +0200

    test: add re-export scenario to test-codebase

---
+++ test-codebase/src/re-export-consumer.ts
@@ -0,0 +1,5 @@
+import { internalHelper } from './re-export-test';
+
+export function useHelper(): string {
+  return internalHelper();
+}
---
+++ test-codebase/src/re-export-test.ts
@@ -0,0 +1,9 @@
+function internalHelper(): string {
+  return "helper";
+}
+
+function anotherInternal(): number {
+  return 42;
+}
+
+export { internalHelper, anotherInternal };
